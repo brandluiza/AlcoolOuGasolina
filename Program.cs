32da10790ef110357ada76be6da19e795f4396d0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-04 21:38:06 -0300

    finalizar o programa

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿﻿Console.Clear();
+
+    
+ Console.WriteLine("--- Etanol ou Gasolina? ---\n");
+
+Console.Write("Digite o preço do etanol (R$).....: ");
+decimal precoEtanol = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.Write("Digite o preço da gasolina (R$)...: ");
+decimal precoGasolina = Convert.ToDecimal(Console.ReadLine());
+
+double razao = CalcularRazao(precoEtanol, precoGasolina);
+ bool usarGasolina = DeveAbastecerComGasolina(razao);
+ 
+
+Console.WriteLine();
+Console.WriteLine("O preço do etanol corresponde a " + (razao * 100).ToString("N1") + "% do preço da gasolina.");
+
+if (usarGasolina)
+{ Console.WriteLine("Recomendação: Abasteça com GASOLINA.");
+}
+ 
+else
+{
+    Console.WriteLine("Recomendação: Abasteça com ETANOL.");
+}
+    
+    
+static double CalcularRazao(decimal etanol, decimal gasolina)
+
+
+{
+return (double)(etanol / gasolina);
+}
+
+static bool DeveAbastecerComGasolina(double razao)
+
+
+ {double limite = 0.73;
+  return razao >= limite;}
+
+    
